0a7888e98690cdbc8b6db64ac4fa39033995c68e
Liliana Tuta <lilianatutza@gmail.com>
2024-06-26 18:40:58 +0100

    inserted h1, targeted the h1 in JS and styled h1 from JS

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ function handleClick() {
 let buttonElement = document.querySelector("#special-button");
 buttonElement.addEventListener("click", handleClick);
 
-buttonElement.style.background ="red"
+buttonElement.style.background = "red"
+
+let heading = document.querySelector("h1");
+heading.style.color = "green"
