c0701558c0fd1fc458767a75905e88348b365b67
Liliana Tuta <lilianatutza@gmail.com>
2024-06-26 18:46:03 +0100

    removed some attributes form html; changed the JS
    moved the h1 function to now apply the color change to h1 when we click on the button

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function handleClick() {
   alert("hi");
+  
+let heading = document.querySelector("h1");
+heading.style.color = "green"
 }
 
 let buttonElement = document.querySelector("#special-button");
 buttonElement.addEventListener("click", handleClick);
 
-buttonElement.style.background = "red"
 
-let heading = document.querySelector("h1");
-heading.style.color = "green"
