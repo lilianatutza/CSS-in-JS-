44bb9e0d5591663f35ae0137afe612a934bfcc0e
Liliana Tuta <lilianatutza@gmail.com>
2024-06-26 18:52:54 +0100

    More styling from JS
    border-radius becomes borderRadius in JS

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function handleClick() {
   alert("hi");
-  
-let heading = document.querySelector("h1");
-heading.style.color = "green"
+
+  let heading = document.querySelector("h1");
+  heading.style.color = "green";
+  heading.style.background = "yellow";
+  heading.style.borderRadius = "50px";
 }
 
 let buttonElement = document.querySelector("#special-button");
 buttonElement.addEventListener("click", handleClick);
-
-
