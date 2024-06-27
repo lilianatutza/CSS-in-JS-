6db7ab4828a5f05699dc9d20bb6383e03d378fa5
Liliana Tuta <lilianatutza@gmail.com>
2024-06-27 10:00:13 +0100

    manipulated the padding f the button from JS

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 function handleClick() {
   alert("hi");
-
-  let heading = document.querySelector("h1");
-  heading.style.color = "green";
-  heading.style.background = "yellow";
-  heading.style.borderRadius = "50px";
 }
 
 let buttonElement = document.querySelector("#special-button");
 buttonElement.addEventListener("click", handleClick);
+buttonElement.style.padding = "100px";
