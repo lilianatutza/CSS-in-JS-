1f40822bb241af1435ccc4da76192cd4c9f6e9b5
Liliana Tuta <lilianatutza@gmail.com>
2024-06-27 10:47:19 +0100

    recap
    less used
    //setAttributes
    //element.style.marginRight = "10px"
    
    commonly used and recommended to be used
    //element.classList.add("special")
    //element.classList.remove("special")
    //element.classList.toggle("special")

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ function handleClick() {
 
 let buttonElement = document.querySelector("#special-button");
 buttonElement.addEventListener("click", handleClick);
+
+
+//setAttributes
+//element.style.marginRight = "10px"
+//element.classList.add("special")
+//element.classList.remove("special")
+//element.classList.toggle("special")
